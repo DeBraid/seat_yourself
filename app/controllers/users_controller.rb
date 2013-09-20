879efafa8c4084dcae022046470ab25564fc6a16
AhmedNadar <ahmedfay@hotmail.com>
2013-09-20 15:39:28 -0400

    Add strong params for User controller and remov that from the modle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:email, :password, :password_confirmation, :full_name)
+  end
 end
